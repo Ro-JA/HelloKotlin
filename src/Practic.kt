0cc28d98a112050d2c893ec8797f5b8e66de06ef
romanboyarkin <boyra75.rb@gmail.com>
2022-03-28 11:13:04 +0900

    add

---
+++ src/Practic.kt
@@ -0,0 +1,12 @@
+import java.awt.Color
+
+fun main() {
+    var rainbowColor = "#330000"
+    val blackColor = "#000000"
+
+    rainbowColor = "#FFFFFF"
+
+    print(rainbowColor + blackColor)
+
+
+}
